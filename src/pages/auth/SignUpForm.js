b68b6e457a7b06a3a71f0ab2f47c3cc8dc7203ef
MerelMoehn <merelmoehn@outlook.com>
2023-02-22 11:50:09 +0000

    Feat: create handlechange and handlesubmit for signupform

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,13 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
 import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+import axios from "axios";
 
 const SignUpForm = () => {
+    // This code is based on the Code Institute Walkthrough project: Moments
+    const [signUpData, setSignUpData] = useState({
+        username: "",
+        password1: "",
+        password2: "",
+      });
+    const { username, password1, password2 } = signUpData;
+    const history = useHistory();
+
+    const handleChange = (event) => {
+        setSignUpData({
+          ...signUpData,
+          [event.target.name]: event.target.value,
+        });
+      };
+
+      const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+          await axios.post("/dj-rest-auth/registration/", signUpData);
+          history.push("/signin");
+        } catch (err) {
+          setErrors(err.response?.data);
+        }
+      };
+
   return (
     <Row>
       <Col md={6} className={`my-auto d-none d-md-block ${styles.SignUpCol}`}>
@@ -22,18 +49,18 @@ const SignUpForm = () => {
         <Container className={`${appStyles.Content} p-4`}>
           <h1 className={styles.Header}>sign up</h1>
 
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group controlId="username">
               <Form.Label className="d-none">username</Form.Label>
-              <Form.Control className={styles.Input} type="text" placeholder="Enter Username" name="username" />
+              <Form.Control className={styles.Input} type="text" placeholder="Enter Username" name="username" value={username} onChange={handleChange} />
             </Form.Group>
             <Form.Group controlId="password1">
               <Form.Label className="d-none">password1</Form.Label>
-              <Form.Control className={styles.Input} type="password1" placeholder="Enter Password" name="password1" />
+              <Form.Control className={styles.Input} type="password" placeholder="Enter Password" name="password1" value={password1} onChange={handleChange}/>
             </Form.Group>
             <Form.Group controlId="password2">
               <Form.Label className="d-none">password2</Form.Label>
-              <Form.Control className={styles.Input} type="password" placeholder="Confirm Password" name="password2" />
+              <Form.Control className={styles.Input} type="password" placeholder="Confirm Password" name="password2" value={password2} onChange={handleChange} />
             </Form.Group>
             <Button className={`${btnStyles.Button} ${btnStyles.Stretch}`} type="submit">
               Sign me Up!
