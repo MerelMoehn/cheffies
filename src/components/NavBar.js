111b1eda954af81192c10da676d39605a30dec17
MerelMoehn <merelmoehn@outlook.com>
2023-02-22 10:31:16 +0000

    Feat: add router for navbar pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,31 @@
-import React from 'react';
-import {Navbar, Container, Nav} from "react-bootstrap";
-import logo from '../assets/logo.png';
+import React from "react";
+import styles from "../styles/NavBar.module.css";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/logo.png";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <Navbar bg="dark" expand="md" fixed="top"><Container>
-  <Navbar.Brand><img src={logo} alt="logo" hieght="45"/></Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="ml-auto text-right">
-      <Nav.Link><i class="fas fa-home"></i>Home</Nav.Link>
-      <Nav.Link>Sign-up</Nav.Link>
-      <Nav.Link>Sign-in</Nav.Link>
-    </Nav>
-  </Navbar.Collapse></Container>
-</Navbar>
-  )
-}
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <NavLink to="/">
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height="45" />
+        </Navbar.Brand>
+        </NavLink>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-right">
+            <NavLink exact to="/" className={styles.NavText} activeClassName={styles.Active}>
+              <i class="fas fa-home"></i>Home
+            </NavLink>
+            <NavLink to="/signup" className={styles.NavText} activeClassName={styles.Active}>Sign-up</NavLink>
+            <NavLink to="/signin" className={styles.NavText} activeClassName={styles.Active}>Sign-in</NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default NavBar
+export default NavBar;
