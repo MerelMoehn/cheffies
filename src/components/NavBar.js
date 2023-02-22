427302d9e5411d244c0f856b459ab8c4f77c1661
MerelMoehn <merelmoehn@outlook.com>
2023-02-22 19:06:10 +0000

    Style: transform navbar links into dropdown

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,11 @@
 import styles from "../styles/NavBar.module.css";
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
 
@@ -20,42 +23,65 @@ const NavBar = () => {
     }
   };
 
-  const addRecipeIcon = (
-    <NavLink
-    to="/recipes/create"
-    className={styles.NavText}
-    activeClassName={styles.Active}
-  >
-    <i className="far fa-plus-square"></i>Add recipe
-  </NavLink>
-  )
-  const loggedInIcons = <>
-  <NavLink
-        className={styles.NavLink}
-        activeClassName={styles.Active}
-        to="/feed"
-      >
-        <i className="fas fa-stream"></i>Feed
-      </NavLink>
+  const loggedInIcons = (
+    <>
       <NavLink
-        className={styles.NavLink}
+        to="/recipes/create"
+        className={styles.NavText}
         activeClassName={styles.Active}
-        to="/liked"
       >
-        <i className="fas fa-heart"></i>Liked
+        <i className="far fa-plus-square"></i>
       </NavLink>
-      <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
-        <i className="fas fa-sign-out-alt"></i>Sign out
-      </NavLink>
+      <NavDropdown
+        title={
+          <span>
+            <i className="fa-solid fa-bars"></i>
+          </span>
+        }
+        id="basic-nav-dropdown"
+      >
+        <NavDropdown.Item>
+          <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/feed"
+          >
+            <i className="fas fa-stream"></i>Feed
+          </NavLink>
+        </NavDropdown.Item>
+        <NavDropdown.Item>
+          <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/liked"
+          >
+            <i className="fas fa-heart"></i>Liked
+          </NavLink>
+        </NavDropdown.Item>
+        <NavDropdown.Item>
+          <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
+            <i className="fas fa-sign-out-alt"></i>Sign out
+          </NavLink>
+        </NavDropdown.Item>
+      </NavDropdown>
       <NavLink
         className={styles.NavLink}
         to={`/profiles/${currentUser?.profile_id}`}
       >
-        <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+        <Avatar src={currentUser?.profile_image} height={40} />
       </NavLink>
-      </>;
+    </>
+  );
   const loggedOutIcons = (
     <>
+      <NavLink
+        exact
+        to="/"
+        className={styles.NavText}
+        activeClassName={styles.Active}
+      >
+        Home
+      </NavLink>
       <NavLink
         to="/signup"
         className={styles.NavText}
@@ -84,15 +110,6 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-right">
-            <NavLink
-              exact
-              to="/"
-              className={styles.NavText}
-              activeClassName={styles.Active}
-            >
-              <i class="fas fa-home"></i>Home
-            </NavLink>
-            {currentUser && addRecipeIcon}
             {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>
