cb0f3402a536ce3d8863afecb578343aadb03111
MerelMoehn <merelmoehn@outlook.com>
2023-03-19 12:04:40 +0000

    Style: fix alert on top of screen

--- src/components/AlertPopup.js
+++ src/components/AlertPopup.js
@@ -1,12 +1,13 @@
 import Alert from 'react-bootstrap/Alert';
 import useAlert from '../hooks/useAlert';
+import styles from "../styles/Alert.module.css";
 
 const AlertPopup = () => {
   const { text, type } = useAlert();
 
   if (text && type) {
     return (
-        <Alert className='mt-2' variant={type}>
+        <Alert className={styles.FixAlert} variant={type}>
             {text}
         </Alert>
     );
